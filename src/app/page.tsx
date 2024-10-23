dbd876d4a5a2b5d9e4466a34a1df4b93cd71b1c0
Rajath Rao <raorajath1998@gmail.com>
2024-10-23 09:52:26 +0530

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>test
+      <h1>Home</h1>
       <Link href="/about">About</Link>
     </div>
   );
